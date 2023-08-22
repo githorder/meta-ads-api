81f89148584d3194af108357eb8ab30b0e324be4
githorder <diyor.baynazarov2000@gmail.com>
2023-08-22 16:05:11 +0500

    feat: add sorting by date and remove reach column

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const AdSet = bizSdk.AdSet;
 const AdReportRun = bizSdk.AdReportRun;
 
 const accessToken =
-  "EAAHXvke4wn4BO4mgotzegGFZAorAgFK4BB8MwzFPcvU5iNc8OiyOGskcVbFQq1HENZCLkiGMiGWMpCqHDPDjhr1kCA14FlUiuosEtdbAu7Bw997yGDKZBYfZCMQp8i4nSaa3NFU9qgv4yVBFLnsQNOXALYNshcHfjAzN5SxevQUNKJy2tVinmIZBZBDczflN0tAOCZAGo5MybiXr0nmWo8vaduyUFOZB";
+  "EAAHXvke4wn4BOZBuOGBSYeJN1vANaMDVmkkVg0riODVN6XkeHt6XPpFRT1opZBy88DnRz43zo454icHweD6hYxd1zgIgW5qHf6OFntKBnN1lELgmEZA0OIHZBV36puiMih0d8DgvgnZAqDooCyulOQYMLRHESx9PRdZAbhmFZB3z1xImt1HMZB6qySYtQuSpVzUd5mNlAFIzxWxg8kMIrU2mryXX9ZBIZD";
 const accountId = "act_473357418251469";
 
 bizSdk.FacebookAdsApi.init(accessToken);
@@ -21,9 +21,12 @@ const account = new AdAccount(accountId);
 
 const params = {
   breakdowns: "age, gender",
-  date_preset: "last_month",
+  // date_preset: "last_month",
+  date_preset: "last_week_mon_sun",
   time_increment: 1,
   action_attribution_windows: ["1d_click", "7d_click", "1d_view"],
+  sort: ["date_start_ascending"],
+  level: "adset",
 };
 
 const fields = [
@@ -31,7 +34,6 @@ const fields = [
   "adset_name",
   "spend",
   "impressions",
-  "reach",
   "frequency",
   "inline_link_clicks",
   "cost_per_inline_link_click",
@@ -41,59 +43,110 @@ const fields = [
 ];
 
 (async function () {
-  const adSets = await account.getAdSets([], {});
-  const adSetIds = [];
+  let insights = await account.getInsights(fields, params);
 
-  for (const adSet of adSets) {
-    adSetIds.push(adSet._data.id);
-  }
+  let notEmpty = true;
 
-  for (let i = 0; i < adSetIds.length; i++) {
-    let insights = await new AdSet(adSetIds[i]).getInsights(fields, params);
-    let notEmpty = true;
+  while (notEmpty) {
+    for (const insight of insights) {
+      const action = insight._data?.actions?.filter(
+        ({ action_type }) => action_type === "landing_page_view"
+      );
+      const costAction = insight._data?.cost_per_action_type?.filter(
+        ({ action_type }) => action_type === "landing_page_view"
+      );
 
-    let j = 0;
-    while (notEmpty) {
-      for (const insight of insights) {
-        const action = insight._data?.actions?.filter(
-          ({ action_type }) => action_type === "landing_page_view"
-        );
-        const costAction = insight._data?.cost_per_action_type?.filter(
-          ({ action_type }) => action_type === "landing_page_view"
-        );
+      await createRecord([
+        insight._data.campaign_name,
+        insight._data.adset_name,
+        insight._data.date_start,
+        insight._data.age ?? "unknown",
+        insight._data.gender ?? "unknown",
+        insight._data.spend ?? "-",
+        "Просмотры целевой страницы",
+        (action && action[0]?.value) ?? "-",
+        (costAction && costAction[0]?.value) ?? "-",
+        insight._data.inline_link_clicks ?? "-",
+        insight._data.cost_per_inline_link_click ?? "-",
+        (insight._data.website_ctr && insight._data.website_ctr[0].value) ??
+          "-",
+        insight._data.impressions ?? "-",
+        insight._data.frequency ?? "-",
+        (action && action[0]?.value) ?? "-",
+        (costAction && costAction[0]?.value) ?? "-",
+        insight._data.date_start,
+        insight._data.date_start,
+      ]);
+    }
 
-        await createRecord([
-          insight._data.campaign_name,
-          insight._data.adset_name,
-          insight._data.date_start,
-          insight._data.age ?? "unknown",
-          insight._data.gender ?? "unknown",
-          insight._data.spend ?? "-",
-          "Просмотры целевой страницы",
-          (action && action[0]?.value) ?? "-",
-          (costAction && costAction[0]?.value) ?? "-",
-          insight._data.inline_link_clicks ?? "-",
-          insight._data.cost_per_inline_link_click ?? "-",
-          (insight._data.website_ctr && insight._data.website_ctr[0].value) ??
-            "-",
-          insight._data.reach ?? "-",
-          insight._data.impressions ?? "-",
-          insight._data.frequency ?? "-",
-          (action && action[0]?.value) ?? "-",
-          (costAction && costAction[0]?.value) ?? "-",
-          insight._data.date_start,
-          insight._data.date_start,
-        ]);
-      }
+    notEmpty = insights.hasNext();
 
-      notEmpty = insights.hasNext();
-      
-      if (notEmpty) {
-        await sleep(1000);
-        insights = await insights.next();
-      } else {
-        break;
-      }
+    if (notEmpty) {
+      await sleep(1000);
+      insights = await insights.next();
+    } else {
+      break;
     }
   }
 })();
+
+// version without sorting
+
+// (async function () {
+//   const adSets = await account.getAdSets([], {});
+//   const adSetIds = [];
+
+//   for (const adSet of adSets) {
+//     adSetIds.push(adSet._data.id);
+//   }
+
+//   for (let i = 0; i < adSetIds.length; i++) {
+//     let insights = await new AdSet(adSetIds[i]).getInsights(fields, params);
+//     let notEmpty = true;
+
+//     let j = 0;
+//     while (notEmpty) {
+//       for (const insight of insights) {
+//         const action = insight._data?.actions?.filter(
+//           ({ action_type }) => action_type === "landing_page_view"
+//         );
+//         const costAction = insight._data?.cost_per_action_type?.filter(
+//           ({ action_type }) => action_type === "landing_page_view"
+//         );
+
+//         console.log(insight._data);
+
+//         await createRecord([
+//           insight._data.campaign_name,
+//           insight._data.adset_name,
+//           insight._data.date_start,
+//           insight._data.age ?? "unknown",
+//           insight._data.gender ?? "unknown",
+//           insight._data.spend ?? "-",
+//           "Просмотры целевой страницы",
+//           (action && action[0]?.value) ?? "-",
+//           (costAction && costAction[0]?.value) ?? "-",
+//           insight._data.inline_link_clicks ?? "-",
+//           insight._data.cost_per_inline_link_click ?? "-",
+//           (insight._data.website_ctr && insight._data.website_ctr[0].value) ??
+//             "-",
+//           insight._data.impressions ?? "-",
+//           insight._data.frequency ?? "-",
+//           (action && action[0]?.value) ?? "-",
+//           (costAction && costAction[0]?.value) ?? "-",
+//           insight._data.date_start,
+//           insight._data.date_start,
+//         ]);
+//       }
+
+//       notEmpty = insights.hasNext();
+
+//       if (notEmpty) {
+//         await sleep(1000);
+//         insights = await insights.next();
+//       } else {
+//         break;
+//       }
+//     }
+//   }
+// })();
